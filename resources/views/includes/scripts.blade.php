bb25804ff31c77f1745112ef5eec0a2773095511
Gisan Geff <ggranraniego@gmail.com>
2020-08-12 21:36:31 +0800

    Layouts blade code refactoring

---
+++ resources/views/includes/scripts.blade.php
@@ -0,0 +1,9 @@
+<!-- jQuery -->
+<script src="/assets/vendor/jquery/jquery.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="/assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!-- Contact Form JavaScript -->
+<script src="/assets/js/jqBootstrapValidation.js"></script>
+<script src="/assets/js/contact_me.js"></script>
+<!-- Theme JavaScript -->
+<script src="/assets/js/clean-blog.min.js"></script>
